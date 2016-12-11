56722e8855f43fe24d8a1e40025b7ee9a9ee24b0
Daniel <eldan3k@gmail.com>
2016-12-11 13:20:20 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,8 +19,13 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
+   
+     get("/hello", (request, response) -> {
+         Map<String, Object> attributes = new HashMap<>();
+         attributes.put("message", "Hello World!");
 
-    get("/hello", (req, res) -> "Hello World");
+            return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
